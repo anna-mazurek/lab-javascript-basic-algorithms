c23a04d82bf78ab1e88cc79928d5686355ecbcad
Anna Mazurek <anna-mazurek@hotmail.com>
2021-01-11 23:06:35 +0100

    Adding bonus exercise

--- js/index.js
+++ js/index.js
@@ -36,5 +36,19 @@ if (hacker1 < hacker2) {
     console.log(`What?! You both have the same name?`);
   }
 
+// Bonus 1
 
+var loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec interdum odio, non mattis neque. Duis vulputate mi ac laoreet elementum. Aenean at accumsan odio. Proin sit amet dignissim ipsum, convallis commodo odio. Nunc non est vehicula, lobortis justo vel, commodo quam. Suspendisse ultricies justo felis, scelerisque scelerisque quam gravida vitae. Etiam sed fringilla nisi. Sed non auctor lectus, id lobortis quam. Etiam eget varius urna. Aenean enim libero, eleifend eu consectetur a, tempus sit amet nibh. Fusce dapibus tristique mi sed ullamcorper. Mauris ut leo in ipsum bibendum fringilla. Pellentesque ut sagittis sapien. Pellentesque placerat mauris accumsan pharetra suscipit. Pellentesque dui ipsum, eleifend sed rutrum a, molestie a arcu. Sed ac augue vestibulum nisi molestie feugiat. Aliquam augue sem, consectetur eget magna sed, egestas ornare sapien. Suspendisse aliquet leo metus, eu finibus dui lacinia eget. Fusce eros nisl, rutrum in malesuada ultrices, pharetra semper tellus. Suspendisse nibh est, placerat nec luctus sit amet, lacinia quis ipsum. Maecenas ac elit accumsan, porta enim sed, vulputate diam. Praesent ultricies nisl sit amet commodo imperdiet. Aliquam neque dolor, feugiat quis dolor at, laoreet condimentum odio. Aliquam erat volutpat. Donec quis rhoncus nisi. In at tincidunt neque. Quisque porttitor, ipsum at vulputate facilisis, orci mi semper est, et suscipit leo mi a sapien. Sed orci urna, euismod sit amet ullamcorper quis, euismod a leo. Curabitur tempor sollicitudin lacus sit amet tempor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam a diam et magna ornare congue. Donec condimentum felis tortor, vel lobortis purus dignissim ut. Nulla in imperdiet mauris, ac rutrum augue. Nulla a turpis vitae elit placerat sagittis eu non diam. Phasellus ultrices tellus et ultrices molestie. Aliquam vehicula elementum nunc, ut sodales lorem luctus sit amet.";
+
+function wordCounter(text) {
+  return text.split(' ').length;
+}
+
+console.log(wordCounter(loremIpsum));
+
+function etCounter(text) {
+  return text.split('et').length;
+}
+
+console.log(etCounter(loremIpsum));
 
